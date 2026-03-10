4f66dde47f885db9d6a975d420ba42ab17821add
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-03-09 21:59:03 -0600

    Se agrego la configuracion de todo el microservicio de EmpresasCoperex

---
+++ empresasCoperex/configs/app.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { dbConnection } from './db.js';
+import { corsOptions } from './cors-configuration.js';
+
+const BASE_PATH = '/gestorEmpresas/v1';
+
+const middlewares = (app) => {
+    app.use(express.json());
+    app.use(cors(corsOptions));
+    app.use(morgan('dev'));
+}
+
+const routes = (app) => {
+
+    app.get(`${BASE_PATH}/Health`, (request, response) => {
+        response.status(200).json({
+            status: 'Healthy',
+            timestamp: new Date().toISOString(),
+            service: 'GestorDeEmpresas Server'
+        })
+    })
+
+    app.use((req, res) => {
+        res.status(404).json({
+            success: false,
+            message: 'Endpoint no encontrado en la API'
+        })
+    })
+}
+
+
+export const initServer = async () => {
+    const app = express();
+    const PORT = process.env.PORT;
+    app.set('trus proxy', 1);
+
+    try {
+        await dbConnection();
+        middlewares(app);
+        routes(app);
+
+        app.listen(PORT, () => {
+            console.log(`GestorDeEmpresas Server running on port ${PORT}`);
+            console.log(`Health check: http://localhost:${PORT}${BASE_PATH}/health`);
+        });
+
+    } catch (error) {
+        console.error(`Error starting Admin Server: ${error.message}`);
+        process.exit(1);
+    }
+}
